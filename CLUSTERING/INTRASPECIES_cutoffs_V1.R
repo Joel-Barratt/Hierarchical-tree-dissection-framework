4faea3bbd6850e4638a66ee5375e1dbaa1c8c77f
Joel Barratt <52165459+Joel-Barratt@users.noreply.github.com>
2023-04-24 17:21:57 -0400

    Update INTRASPECIES_cutoffs_V1.R

--- CLUSTERING/INTRASPECIES_cutoffs_V1.R
+++ CLUSTERING/INTRASPECIES_cutoffs_V1.R
@@ -1,23 +1,14 @@
 
-#### THE FOLLOWING PART YOU REPEAT A THOUSAND TIMES SO THAT YOU CAN GET AN ACCURATE AVERAGE CUTOFF FROM MANY REPLICATES
-#### otherwise you may get bouncy specimens again and the epis dont really like that. Paralellize it so it doesn't take a decade.
-
 number_of_species = as.numeric(readLines("SPECIES"))
 
-## select the range of clusters to check for the maximum number of clusters that has at least 2 specimens in it (no less).
+## The ranges below are for identifying iterations of M2. This means the tree will be dissected at 5 clusters through 1000 clusters for each population to find
+## which value results in partitions where each partition has 2 or more genotypes/isolates in it.
 min_cluster_range <- 5
 max_cluster_range <- 1000
 
-#bootstraps <- 1000 # number of times to run the code in order to calculate an average cutoff
 bootstraps = as.numeric(readLines("BOOTSTRAPS"))
 
-### first we need to select and import the most recent matrix -- DO WE STILL NEED THESE NEXT FEW LINES OF HASHED OUT CODE.
-#matrix_location <- readLines("MATRIX_LOCATION")
-
 setwd(matrix_location)
-#myFiles <- list.files(pattern="*csv", all.files = FALSE)
-#myMatrix <- (sort(myFiles, decreasing = TRUE)[1])
-#matrix <- as.matrix(read.table(myMatrix, sep = ",", row.names=1, header=TRUE))
 
 
 setwd("../CLUSTERING/TMP_REP/")
@@ -26,20 +17,17 @@ number_of_threads = as.numeric(readLines("THREADS"))
 
 
 print(paste0("Now testing a range of cluster numbers from ", min_cluster_range, " to ", max_cluster_range, " to identify the maximum cluster number where no singletons exist in their own cluster."))
-## Cluster_distance_matrix
-Ensemble_x <- as.hclust(agnes(x=matrix, diss = TRUE, stand = TRUE, metric = "manhattan", method = "ward"))
+
+## Cluster distance matrix using wards method.
+Ensemble_x <- as.hclust(agnes(x=matrix, diss = TRUE, stand = TRUE, method = "ward"))
 
 pick_min_cluster_number <- c()
 
 ###just_for strongy data we can use max_cluster_range <- 200
 
 for (k in min_cluster_range:max_cluster_range){
-#for (k in 50){
 
 cluster <- as.data.frame(melt(factor(cutree(Ensemble_x, k=k))))
-
-#cluster <- as.data.frame(melt(factor(cutree(Ensemble_x, k=48))))
-#generate data frame showing cluster membership.
 
 cluster_good <- cbind(rownames(cluster), cluster)
 colnames(cluster_good) <- c("Seq_ID", "Cluster")
@@ -74,12 +62,8 @@ foobar <- cluster_good %>% filter(str_detect(Cluster_name, paste0("Cluster_",i,"
 
 number_of_clusters <- min(pick_min_cluster_number[,1])
 
-
-
-
-# now we need a loop to determine the number of specimens in the smallest cluster
-# just in case we can select more than 2 specimens in the next step. If we can, then we should for the sake of diversity.
-# This value will usually be 2 however.
+# now we need a loop to determine the number of genotypes in the smallest cluster.
+# This  will usually be 2 genotypes.
 
 cluster <- as.data.frame(melt(factor(cutree(Ensemble_x, k= number_of_clusters))))
 size_of_each_cluster <- vector()
@@ -95,13 +79,13 @@ size_of_each_cluster <- append(size_of_each_cluster,length(current_cluster$value
 smallest_cluster_size <- min(size_of_each_cluster)
 
 
-
-
 species_clusters <- as.data.frame(melt(factor(cutree(Ensemble_x, k=number_of_species)))) # this is from the original large matrix - the Ensemble_x variable.
 species_clusters <- cbind(species_clusters, c(rownames(species_clusters)))
 colnames(species_clusters) <- c("value", "Seq_ID") ## this will be used later.
 species_clusters_ASHFORDI <- species_clusters
 species_clusters_CAYETANENSIS <- species_clusters
+
+
 #extract patristic distances from your tree Ensemble_x
 PatristicDistMatrix <-as.matrix(cophenetic(Ensemble_x))
 
